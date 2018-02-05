87bb871bb9b8f828ea5dd7246957c50ea2e4e0f7
Meir Naiman <meirnaiman@gmail.com>
2018-02-05 00:04:03 -0500

    Created Fits assignment file

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,23 +1,22 @@
 class ListDivideException(Exception):
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    pass
 
-def listDivide( numbers, divide = 2):
+def listDivide(numbers, divide = 2):
     count = 0
     for x in numbers:
-        if x % divide is 0:
+        if x % divide == 0:
             count += 1
     return count
 
-
 def testListDivide():
-    listDivide('hi')
-    listDivide([2,4,6,8,10])
-    listDivide([30, 54, 63,98, 100], divide=10)
-    listDivide([])
-    listDivide([1,2,3,4,5], 1)
+    try:
+        listDivide([1,2,3,4,5])
+        listDivide([2,4,6,8,10])
+        listDivide([30, 54, 63,98, 100], divide=10)
+        listDivide(['hi'])
+        listDivide([1,2,3,4,5], 1)
+    except:
+        raise ListDivideException
 
-try:
-    testListDivide()
-except:
-    raise ListDivideException('Error')
+testListDivide()
+
